a0d705840b9860302c1e170a0718251cb5cf8746
PAIN\Admin <Oleksandr.kysil@sixa.io>
2018-04-12 12:31:03 +0300

    First test commit

---
+++ FilreRead/src/test/java/helpMethods/StartFinishFixture.java
@@ -0,0 +1,19 @@
+package helpMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+
+public class StartFinishFixture {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeTest
+    public void startFixture() {
+
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 15);
+    }
+    
+}
