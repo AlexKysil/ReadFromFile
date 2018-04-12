48e3dd7b6a9c8ba6b0a9c7cb2653fe45c25af1d8
PAIN\Admin <Oleksandr.kysil@sixa.io>
2018-04-12 14:29:42 +0300

    Fixed Fileread in method: "readFileAndWriteToList" Fixed Filepath in class ReadFile

--- FilreRead/src/test/java/code/ReadFromFile.java
+++ FilreRead/src/test/java/code/ReadFromFile.java
@@ -1,4 +1,20 @@
 package code;
 
+import helpMethods.ReadFile;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class ReadFromFile {
+public static void main(String[] args){
+    ReadFile firstLook = new ReadFile();
+
+    String[] list = firstLook.readFileAndWriteToList();
+    for(String s : list){
+        System.out.println(s);
+    }
+
+
+
+}
 }
--- FilreRead/src/test/java/helpMethods/ReadFile.java
+++ FilreRead/src/test/java/helpMethods/ReadFile.java
@@ -1,14 +1,16 @@
 package helpMethods;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class ReadFile {
 
     private String wayToTheFile;
-    private  ArrayList<String> listOFWebSites;
+    private static ArrayList<String> listOFWebSites;
 
-    ReadFile(){
-        wayToTheFile = "SCR_Files/websites.txt";
+    public ReadFile(){
+        String path = new File("").getAbsolutePath();
+        wayToTheFile = path + "\\FilreRead\\src\\test\\java\\SCR_Files\\websites.txt";
     }
     ReadFile(String wayToTheFile){
         this.wayToTheFile = wayToTheFile;
@@ -26,7 +28,38 @@ public class ReadFile {
         return listOFWebSites;
     }
 
-    public void readFileAndWriteToList(){
+    public void readFileAndWriteToList() {
+    FileInputStream fis = null;
+    try{
+        fis = new FileInputStream(wayToTheFile);
+    }catch(FileNotFoundException e) {
+        e.printStackTrace();
+    }
+
+    byte[] content = new byte[0];
+    try{
+        content = new byte[fis.available()];
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+    try{
+        fis.read(content);
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+
+    try{
+        fis.close();
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+
+    String[] lines = new String[0];
+    try{
+        lines = new String(content,"Cp1251").split("\n");
+    }catch (UnsupportedEncodingException e){
+        e.printStackTrace();
+    }
         
     }
 }
